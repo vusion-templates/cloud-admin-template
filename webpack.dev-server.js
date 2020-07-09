63be825e13fbd1967cf3caa2526c6594a8137d90
zhangsanshi <zhangsanshi5566@gmail.com>
2020-07-09 17:28:46 +0800

    feat: support gateway isolation

--- webpack.dev-server.js
+++ webpack.dev-server.js
@@ -1,7 +1,33 @@
 const host = 'localhost';
 const path = require('path');
 const pkg = require('./package.json');
+const platformConfig = require('./platform.config.json');
+if (!platformConfig.tenantId || !platformConfig.projectId) {
+    console.error('parse platform.config error');
+    process.exit(1);
+}
+const onProxyReq = function (proxyReq, req, res) {
+    proxyReq.removeHeader('x-forwarded-proto');
+    proxyReq.removeHeader('x-forwarded-host');
+    proxyReq.removeHeader('x-forwarded-port');
+    proxyReq.removeHeader('x-forwarded-for');
 
+    const cookies = {};
+    let cookie = proxyReq.getHeader('cookie');
+    cookie = typeof cookie === 'string' ? cookie.split('; ') : cookie;
+    if (Array.isArray(cookie)) {
+        cookie.forEach((item) => {
+            const arr = item.split('=');
+            if (arr.length === 2)
+                cookies[arr[0].toLowerCase()] = arr[1].trim();
+        });
+    }
+    cookies.authorization && proxyReq.setHeader('authorization', cookies.authorization);
+    cookies.username && proxyReq.setHeader('username', cookies.username);
+    proxyReq.setHeader('DomainName', pkg.name.replace(/-client$/, ''));
+
+    // console.log(proxyReq.path, proxyReq.getHeaders());
+};
 module.exports = function (port) {
     return {
         host,
@@ -22,28 +48,13 @@ module.exports = function (port) {
                 target: 'http://api.gateway.lowcode',
                 changeOrigin: true,
                 autoRewrite: true,
-                onProxyReq(proxyReq, req, res) {
-                    proxyReq.removeHeader('x-forwarded-proto');
-                    proxyReq.removeHeader('x-forwarded-host');
-                    proxyReq.removeHeader('x-forwarded-port');
-                    proxyReq.removeHeader('x-forwarded-for');
-
-                    const cookies = {};
-                    let cookie = proxyReq.getHeader('cookie');
-                    cookie = typeof cookie === 'string' ? cookie.split('; ') : cookie;
-                    if (Array.isArray(cookie)) {
-                        cookie.forEach((item) => {
-                            const arr = item.split('=');
-                            if (arr.length === 2)
-                                cookies[arr[0].toLowerCase()] = arr[1].trim();
-                        });
-                    }
-                    cookies.authorization && proxyReq.setHeader('authorization', cookies.authorization);
-                    cookies.username && proxyReq.setHeader('username', cookies.username);
-                    proxyReq.setHeader('DomainName', pkg.name.replace(/-client$/, ''));
-
-                    // console.log(proxyReq.path, proxyReq.getHeaders());
-                },
+                onProxyReq,
+            },
+            '^/gw/': {
+                target: `http://${platformConfig.tenantId}-${platformConfig.projectId}.api.gateway.lowcode`,
+                changeOrigin: true,
+                autoRewrite: true,
+                onProxyReq,
             },
         },
     };
