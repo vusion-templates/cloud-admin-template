70648a17109d9468ae3e2d25965f35dc0b08dbd2
zhangxiaoyu06 <zhangxiaoyu06@corp.netease.com>
2020-09-05 15:37:05 +0800

    feat: add apollo gql

---
+++ src/global/features/apollo/client.js
@@ -0,0 +1,29 @@
+// Create a GraphQL schema and resolvers
+// 生成方式需要使用这里导出的方法
+import { makeExecutableSchema } from '@graphql-tools/schema';
+// Setup Apollo client as usual, but use SchemaLink
+import { SchemaLink } from 'apollo-link-schema';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from 'apollo-client';
+// TODO import Query  and resolvers file for projects
+import typeDefs from './index.gql';
+
+const resolvers = {
+    Query: {
+        queryAll: () => ({ name: 'women', value: 'www' }),
+        // Or make a remote API call
+    },
+};
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
+
+
+// define our apolloclient
+export const apolloClient = new ApolloClient({
+    link: new SchemaLink({ schema }),
+    cache: new InMemoryCache(),
+});
+
