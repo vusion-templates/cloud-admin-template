e54a04317a4a77aa19e9daaada2630b497d67966
zhangxiaoyu06 <zhangxiaoyu06@corp.netease.com>
2020-10-15 14:53:58 +0800

    fix: use stringify to transform query object

--- src/global/features/apollo/queryStrCollect.js
+++ src/global/features/apollo/queryStrCollect.js
@@ -1,16 +1,5 @@
+import { stringify } from 'qs';
 
-/**
- * 参数是数组的情况需要处理
- * 支持 val 结果包含变量，并且传递给 query 里面的 query 参数
- */
-function objectToQuerystring(obj = {}) {
-    return Object.keys(obj).reduce((str, key, i) => {
-        const delimiter = (i === 0) ? '' : '&';
-        key = encodeURIComponent(key);
-        const val = obj[key] ? encodeURIComponent(obj[key]) : '';
-        return [str, delimiter, key, '=', val].join('');
-    }, '');
-}
 /**
  * 根据实体解析动态的 endpoint
  */
@@ -34,7 +23,7 @@ export default {
                             // key 如果是保留的关键字， query，需要转化成 string 提供给后端
                             let value = variables[key];
                             if (key === 'query') {
-                                value = objectToQuerystring(value);
+                                value = stringify(value);
                             }
                             newVariables[`Query__${operationName}__${key}`] = value;
                         });
