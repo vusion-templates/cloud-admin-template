1992f38b268b01a564455aeb94dce12e71da8c5d
zhangxiaoyu06 <zhangxiaoyu06@corp.netease.com>
2020-11-04 23:19:08 +0800

    fix: 403 error & contain local cookie

--- src/global/features/apollo/client.js
+++ src/global/features/apollo/client.js
@@ -2,11 +2,39 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import ApolloClient from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
+import axios from 'axios';
+
+const formatContentType = function (contentType, data) {
+    const map = {
+        'application/x-www-form-urlencoded'(data) {
+            return JSON.stringify(data);
+        },
+    };
+    return map[contentType] ? map[contentType](data) : data;
+};
+
+const requester = function (chosenURI, options) {
+    const { headers = {}, params, baseURL = '', method = 'POST', body = {}} = options;
+    headers['Content-Type'] = headers['Content-Type'] || 'application/json';
+    const req = axios({
+        params,
+        baseURL,
+        method,
+        url: chosenURI,
+        data: formatContentType(headers['Content-Type'], body),
+        headers,
+        withCredentials: !baseURL,
+        xsrfCookieName: 'csrfToken',
+        xsrfHeaderName: 'x-csrf-token',
+    });
+    return req;
+};
 
 const link = createHttpLink({
     fetchOptions: {
-        method: 'POST',
+        method: 'POST'
     },
+    fetch: requester
 });
 
 // define our apolloclient
