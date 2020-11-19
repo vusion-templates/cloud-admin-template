81b6adb452993f6dbd06905838f6105d92ab5bcf
zxydaisy <1033021372@qq.com>
2020-11-19 20:15:54 +0800

    feat: support transforSchemaWithDataTypes in template

--- src/global/features/dataTypes/install.js
+++ src/global/features/dataTypes/install.js
@@ -1,9 +1,22 @@
 import enums from '../../enums';
+import dataTypes from '../../dataTypes';
+import generate from '@babel/generator';
+import { genInitData } from './tools';
+import { parse, stringify } from './circularJSON';
 
 export default {
     install(Vue, options = {}) {
         Vue.prototype.$global = {
             userInfo: {},
+        };
+
+        // read datatypes from template, then parse schema
+        Vue.prototype.$transforSchemaWithDataTypes = (schema) => {
+            const dataTypesMap = parse(stringify(dataTypes))['dataTypesMap']; // TODO 统一为  dataTypesMap
+            const expressDataTypeObject = genInitData(schema || {}, dataTypesMap);
+            const expression = generate(expressDataTypeObject).code;
+            console.info('expression', expression);
+            return expression;
         };
 
         Vue.prototype.$enums = (key, value) => {
